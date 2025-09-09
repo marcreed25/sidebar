2fe21c1fb4a857b2ddb84bcfeedc7b7b2402cb74
Marc Reed <marcreed7@outlook.com>
2025-09-09 20:04:15 +0100

    Start fixing toggle on mobile

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 var toggle = true;
+var sidebar = document.getElementById('sidebar');
+
+let mode = window.getComputedStyle(document.body).getPropertyValue('--mode');
+
+// Set initial state of sidebar
+if (mode == 'green' || mode == 'orange') {
+    // On desktop or tablet
+    alert('d/t');
+    sidebar.setAttribute('open', 'true'); 
+} else {
+    // On mobile
+    alert('m');
+    sidebar.setAttribute('open', 'false');
+}
 
 function toggleBar() {
-    let sidebar = document.getElementById('sidebar');
     let nav = document.getElementsByTagName('nav')[0];
     let article = document.getElementsByTagName('article')[0];
 
-    if (toggle) {
+    let mode = window.getComputedStyle(document.body).getPropertyValue('--mode');
+
+    if (toggle) { //('green', 'orange').contains(mode)
         // Hide
-        sidebar.style.width = '5%';
-        article.style.width = 'calc(95%)';
-        nav.style.display = 'none';
+        // sidebar.style.width = '5%';
+        // article.style.width = 'calc(95%)';
+        if (mode == 'green' || mode == 'orange') {
+            // On desktop or tablet
+            nav.style.display = 'none';
+
+            sidebar.setAttribute('open', 'false');
+        } else {
+            // On mobile
+            nav.style.display = 'inline';
+
+            sidebar.setAttribute('open', 'true');
+        }
+
         toggle = false;
     } else if (!toggle) {
         // Show
-        sidebar.style.width = '20%';
-        article.style.width = 'calc(80%)';
-        nav.style.display = 'inline';
+        // sidebar.style.width = '20%';
+        // article.style.width = 'calc(80%)';
+
+        if (mode == 'green' || mode == 'orange') {
+            // On desktop or tablet
+            nav.style.display = 'inline';
+
+            sidebar.setAttribute('open', 'true');
+        } else {
+            // On mobile
+            nav.style.display = 'none';
+
+            sidebar.setAttribute('open', 'false');
+        }
+
         toggle = true;
     }
     // 
